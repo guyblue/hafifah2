dd34bdf8bf2060967bc0cb56e81d2f2aed0cf321
Dana <dana11bg@walla.com>
2021-10-28 11:40:21 +0300

    now it works

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import './css-files/App.css';
+
+function App() {
+  return (
+    <div className="App">
+
+      <header class="app-header">
+        <h1 id="title">TinDog</h1>
+      </header>
+      
+      <body>
+
+        <img id="likeButton" />
+        <img id="mainPic" />
+        <img id="nextPicButton" />
+
+      </body>
+
+    </div>
+  );
+}
+
+export default App;
