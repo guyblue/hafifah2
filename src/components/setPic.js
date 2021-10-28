f7f2a91d8f38b667e7912b34dce6f2b9e23be411
Dana <dana11bg@walla.com>
2021-10-28 14:32:16 +0300

    added documented setPic.js hook

---
+++ src/components/setPic.js
@@ -0,0 +1,11 @@
+/**
+ * this function will be a hook function
+ * that updates the picture everytime it gets used
+ *  const pic = setState(picture , setPicture);
+ * 
+ * setPicture = () =>{
+ *  this.state.pic = JSON.stringify(// random pic link //);
+ * }
+ * 
+ * export default // choose name //;
+ */
