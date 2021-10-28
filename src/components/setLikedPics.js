4aedaab701aa50584a69b6d46dccf95586adc89f
Dana <dana11bg@walla.com>
2021-10-28 14:37:37 +0300

    added documented hook function for liked pics

---
+++ src/components/setLikedPics.js
@@ -0,0 +1,12 @@
+/** a hook function that will set the
+ *  localStorage for the liked pics, and
+ *  update it whenever there's an update to the localStorage
+ * 
+ *  const likedPics = setState(likedPics,setLikedPics);
+ *  
+ *  setLikedPics = () =>{
+ *  likedPics = {};
+ * }
+ * 
+ *  export default //function name//;
+ */
