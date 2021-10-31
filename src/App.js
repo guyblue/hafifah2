b34ed19ab20d5d1d75800cac2e816889557681c0
Dana <dana11bg@walla.com>
2021-10-31 11:01:42 +0200

    minor changes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './css-files/App.css';
 
+import React from 'react';
+
 //import './components/setPic';
 
-import './metirial-pics/heart.png';
-import './metirial-pics/blackX.png';
+// import './metirial-pics/heart.png';
+// import './metirial-pics/blackX.png';
 
 function App() {
 
@@ -18,6 +20,13 @@ function App() {
      */
     if (liked){
       console.log("entered liked");
+    }
+
+    if (!document.getElementById(props).src){
+      document.getElementById(props).src = "https://dog.ceo/api/breed/Germanshepherd/images/random";
+    }
+    else{
+
     }
 
   
@@ -37,7 +46,7 @@ function App() {
         <div id="mainPicOpt">
 
           <img id="likeButton" class="sideButton" src="./metirial-pics/heart.png" alt="likePic" />
-          <img id="mainPic" alt="mainPic" />
+          <img id="mainPic" src="https://dog.ceo/api/breed/Germanshepherd/images/random" alt="mainPic" onLoad={this.nextPic(this.id,null)} />
           <img id="nextPicButton" class="sideButton" src="./metirial-pics/blackX.png" alt="nextPic" />
 
         </div>
