6e8b1f578973ff04105d538c06015df710af33b9
Dana <dana11bg@walla.com>
2021-11-02 09:42:02 +0200

    likesPageButton now does an action(but with Error)

--- src/components/likesPageButton.js
+++ src/components/likesPageButton.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { render } from 'react';
+import LikedPicsPage from '../likedPicsPage.js';
 import '../css-files/App.css';
 
 
@@ -7,7 +8,7 @@ import buttonPic from '../metirial-pics/likes button heart picture.png';
 const likesPageButtonDiv = () =>{
     
     return(
-        <div id="likesPageButtonID" className="likesPageButton">
+        <div id="likesPageButtonID" className="likesPageButton" onClick={()=>render(<LikedPicsPage />)}>
             <img className="likesPageButtonPic" src={buttonPic} alt="buttonPic" />
             <p className="likesButtonText">liked pictures</p>
         </div>
---
+++ src/likedPicsPage.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const LikedPicsPage = () =>{
+
+
+    return(
+        <div className="siteBody">
+            <h1 className="title">Pictures you liked</h1>
+
+
+        </div>
+    )
+}
+
+export default LikedPicsPage;
