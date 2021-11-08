4e280f237f338b4af07962a357e0f9a99c81d11c
Dana <dana11bg@walla.com>
2021-11-08 11:07:48 +0200

    minor changes

--- src/components/displayLikedPics.js
+++ src/components/displayLikedPics.js
@@ -3,8 +3,8 @@ import '../css-files/LikedPicsPageDesign.css';
 
 import Dislike from '../metirial-pics/dislike.png';
 
-const LikedPicsFrames = () =>{
-    const [likedPics , setLikedPics] = useState(JSON.parse(localStorage.getItem('likedPics')));
+const LikedPicsFrames = () => {
+    const [likedPics, setLikedPics] = useState(JSON.parse(localStorage.getItem('likedPics')));
 
     // const removeFromStorage = (link) => {
     //     let changes = [...JSON.parse(localStorage.getItem('likedPics'))];
@@ -31,24 +31,22 @@ const LikedPicsFrames = () =>{
 
     return (
         <div className="likedPicsTable">{
-                likedPics.map((x)=>{return(
-                        <div className="likedPicsFrame" id={"frame/"+x} key={likedPics.indexOf(x)}>
+            likedPics.map((x) => 
+                <div className="likedPicsFrame" id={"frame/" + x} key={likedPics.indexOf(x)}>
 
-                            <img className="likedPicture" id={"picture/"+x} src={x} alt="likedPic" />
-                            <img className="dislikeButton" id={"dislike/"+x} src={Dislike} alt="dislikeButton" onClick={()=>{
-                                let changes = [...JSON.parse(localStorage.getItem('likedPics'))];
-                                changes.splice(changes.indexOf(x), 1);
-                                localStorage.setItem('likedPics', JSON.stringify(changes));
-                                setLikedPics(JSON.parse(localStorage.getItem('likedPics')));
-                            }} />
-                            
-                        </div>
-                        )
-                    }
-                )
-            }
-            </div>
+                    <img className="likedPicture" id={"picture/" + x} src={x} alt="likedPic" />
+                    <img className="dislikeButton" id={"dislike/" + x} src={Dislike} alt="dislikeButton" onClick={() => {
+                        let changes = [...JSON.parse(localStorage.getItem('likedPics'))];
+                        changes.splice(changes.indexOf(x), 1);
+                        localStorage.setItem('likedPics', JSON.stringify(changes));
+                        setLikedPics(JSON.parse(localStorage.getItem('likedPics')));
+                    }} />
+
+                </div>
+            )
+        }
+        </div>
     );
-} 
+}
 
 export default LikedPicsFrames;
