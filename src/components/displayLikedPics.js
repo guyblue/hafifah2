fc9f0eb2361faf9708cc12935cefe0caae5e891f
Dana <dana11bg@walla.com>
2021-11-08 10:26:27 +0200

    removed documented useEffect(because wasn't used)

--- src/components/displayLikedPics.js
+++ src/components/displayLikedPics.js
@@ -29,27 +29,6 @@ const LikedPicsFrames = () =>{
     //     )
     // }
 
-    // useEffect(()=>{
-    //     setNewDiv(
-    //     <div className="likedPicsTable">{()=>{
-    //         likedPics.map((x)=>{return(
-    //             <div className="likedPicsFrame" id={x}>
-
-    //             <img className="likedPicture" id={"picture"+x} src={x} alt="likedPic" />
-    //             <img className="dislikeButton" id={"dislike"+x} src={Dislike} alt="dislikeButton" onClick={()=>{
-    //                 let changes = [...JSON.parse(localStorage.getItem('likedPics'))];
-    //                 changes.splice(changes.indexOf(x), 1);
-    //                 localStorage.setItem('likedPics', JSON.stringify(changes));
-    //                 setLikedPics(JSON.parse(localStorage.getItem('likedPics')));
-    //             }} />
-                
-    //         </div>
-    //         )});
-    //     }}</div>
-    //     );
-    //     return newDiv;
-    // },[likedPics], {newDiv} );
-
 
     return (
         <div className="likedPicsTable">{
