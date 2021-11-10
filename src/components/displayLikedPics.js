73427178349d4cffd39d9c883da41bc8b8c4bf34
Dana <dana11bg@walla.com>
2021-11-10 10:34:57 +0200

    moved dislike action to a function+clatter clean

--- src/components/displayLikedPics.js
+++ src/components/displayLikedPics.js
@@ -6,28 +6,12 @@ import Dislike from '../metirial-pics/dislike.png';
 const LikedPicsFrames = () => {
     const [likedPics, setLikedPics] = useState(JSON.parse(localStorage.getItem('likedPics')));
 
-    // const removeFromStorage = (link) => {
-    //     let changes = [...JSON.parse(localStorage.getItem('likedPics'))];
-    //     changes.splice(changes.indexOf(link), 1);
-    //     localStorage.setItem('likedPics', JSON.stringify(changes));
-    // }
-
-    // const dislike = (link) =>{
-    //     let changes = [...JSON.parse(localStorage.getItem('likedPics'))];
-    //     changes.splice(changes.indexOf(link), 1);
-    //     localStorage.setItem('likedPics', JSON.stringify(changes));
-    //     setLikedPics(JSON.parse(localStorage.getItem('likedPics')));
-    // }
-
-    // const placeFrame = (pic,ID) =>{
-    //     return(
-    //     <div className="likedPicsFrame" id={ID}>
-    //         <img className="likedPicture" src={pic} alt="likedPic" onClick={dislike(pic)}/>
-    //         <img className="dislikeButton" src={Dislike} alt="dislikeButton" />
-    //     </div>
-    //     )
-    // }
-
+    const dislike = (link) =>{
+        let changes = [...JSON.parse(localStorage.getItem('likedPics'))];
+        changes.splice(changes.indexOf(link), 1);
+        localStorage.setItem('likedPics', JSON.stringify(changes));
+        setLikedPics(JSON.parse(localStorage.getItem('likedPics')));
+    }
 
     return (
         <div className="likedPicsTable">{
@@ -36,10 +20,7 @@ const LikedPicsFrames = () => {
 
                     <img className="likedPicture" id={"picture/" + x} src={x} alt="likedPic" />
                     <img className="dislikeButton" id={"dislike/" + x} src={Dislike} alt="dislikeButton" onClick={() => {
-                        let changes = [...JSON.parse(localStorage.getItem('likedPics'))];
-                        changes.splice(changes.indexOf(x), 1);
-                        localStorage.setItem('likedPics', JSON.stringify(changes));
-                        setLikedPics(JSON.parse(localStorage.getItem('likedPics')));
+                        dislike(x);
                     }} />
 
                 </div>
