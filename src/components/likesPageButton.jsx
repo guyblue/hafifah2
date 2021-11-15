d0472534d4e3f6f21ec19155d851be4229e5071c
Dana <dana11bg@walla.com>
2021-11-15 12:27:43 +0200

    TODO:removed documented unused code

--- src/components/likesPageButton.jsx
+++ src/components/likesPageButton.jsx
@@ -2,14 +2,12 @@ import React from 'react';
 import { Link } from "react-router-dom";
 import { Favorite } from '@mui/icons-material';
 import { pink } from '@mui/material/colors';
-// import buttonPic from '../metirial-pics/LikesButtonHeartPicture.png';
 import '../css-files/mainPage.css';
 
 const likesPageButtonDiv = () => {
 
     return (
         <Link className="nextSiteLink" to="/likedPicsPage">
-            {/* <img className="likesPageButtonPic" src={buttonPic} alt="buttonPic" /> */}
             <Favorite className="likesPageButtonPic" sx={{color: pink[500] }} />
             <p className="likesButtonText">liked pictures</p>
         </Link>
