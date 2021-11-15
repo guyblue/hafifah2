12454c383b9a555552696701954e02a5059750b2
Dana <dana11bg@walla.com>
2021-11-15 12:25:24 +0200

    TODO: next page button heart pic changed to icon

--- src/components/likesPageButton.jsx
+++ src/components/likesPageButton.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import { Link } from "react-router-dom";
-import buttonPic from '../metirial-pics/LikesButtonHeartPicture.png';
+import { Favorite } from '@mui/icons-material';
+import { pink } from '@mui/material/colors';
+// import buttonPic from '../metirial-pics/LikesButtonHeartPicture.png';
 import '../css-files/mainPage.css';
 
 const likesPageButtonDiv = () => {
 
     return (
         <Link className="nextSiteLink" to="/likedPicsPage">
-            <img className="likesPageButtonPic" src={buttonPic} alt="buttonPic" />
+            {/* <img className="likesPageButtonPic" src={buttonPic} alt="buttonPic" /> */}
+            <Favorite className="likesPageButtonPic" sx={{color: pink[500] }} />
             <p className="likesButtonText">liked pictures</p>
         </Link>
     )
